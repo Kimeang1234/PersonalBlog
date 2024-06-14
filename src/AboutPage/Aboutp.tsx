3a1ba6c90161b77555fbaaec9819d657a8f056b8
LisaOw <kkimeang@gmail.com>
2024-06-14 08:28:02 +0700

    ttoo

--- src/AboutPage/Aboutp.tsx
+++ src/AboutPage/Aboutp.tsx
@@ -14,7 +14,7 @@ function Aboutp() {
             <h2 className='text-3xl font-display2 font-bold md:text-4xl xl:text-5xl md:pt-2 pb-4'>Me</h2>
             <h3 className='lg:w-1/2 text-base line-clamp-3 md:text-2xl pt-5'>Hello guys, I'm Sa. I'm Cambodian, I'm a student at RUPP or Royal University of Phnom Penh. Nowaday I'm training at One World.Co.LTD aka OW. Astronomy has always fascinated me, but living in Phnom Penh it's hard to see the stars clearly. That's why I'm excited about the backyard astronomy boom! With a little planning, even city dwellers can enjoy the wonders of the universe. Maybe I can convince my friends at OW to join a stargazing party or even set up a telescope on the rooftop!  After all, who wouldn't want to explore the cosmos from our very own Phnom Penh?</h3>
          </div>
-          <button className=' md:py-3 md:text-xl md:px-6 bg-black text-white text-base font-bold py-2 px-3'>Read More</button>
+          <button className=' hover:bg-white hover:text-black md:py-3 md:text-xl md:px-6 bg-black text-white text-base font-bold py-2 px-3'>Read More</button>
         </div>
       </div>
     </div>
--- src/BlogPage/Blogweb.tsx
+++ src/BlogPage/Blogweb.tsx
@@ -1,8 +1,110 @@
+// // 
+
+// import React from 'react'
+// import grayball from '../images/grayball.jpg'
+// import Blogfooter from './Blogfooter'
+// import Pchhor from '../images/Pchhor.jpg'
+// import Header from 'Homepage/Header'
+// import cok from '../images/cok.jpg'
+
+// function Blogweb() {
+//   const blogposts = [
+//     {
+//       title: "How to make a website",
+//       author: "<NAME>",
+//       date: "2021-05-01",
+//       id: 1,
+//       content: "This is a blog post about how to make a website, This is a blog post about how to make a website, This is a blog post about how to make a website",
+//       image: grayball,
+//     },
+//     {
+//       title: "How to make a website",
+//       author: "<NAME>",
+//       date: "2021-05-01",
+//       id: 2,
+//       content: "This is a blog post about how to make a website, This is a blog post about how to make a website, This is a blog post about how to make a website",
+//       image: grayball,
+//     },
+//     {
+//       title: "How to make a website",
+//       author: "<NAME>",
+//       date: "2021-05-01",
+//       id: 3,
+//       content: "This is a blog post about how to make a website, This is a blog post about how to make a website, This is a blog post about how to make a website",
+//       image: grayball,
+//     },
+//     {
+//       title: "How to make a website",
+//       author: "<NAME>",
+//       date: "2021-05-01",
+//       id: 4,
+//       content: "This is a blog post about how to make a website, This is a blog post about how to make a website, This is a blog post about how to make a website",
+//       image: grayball,
+//     },
+//     {
+//       title: "How to make a website This is a blog post about how to make a website",
+//       author: "<NAME>",
+//       date: "2021-05-01",
+//       id: 5,
+//       content: "  This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make, This is a blog post about how to make a website",
+//       image: grayball,
+//     },
+//     {
+//       title: "How to make a website",
+//       author: "<NAME>",
+//       date: "2021-05-01",
+//       id: 6,
+//       content: "This is a blog post about how to make a website, This is a blog post about how to make a website, This is a blog post about how to make a website",
+//       image: grayball,
+//     },
+//   ]
+
+//   return (
+//     <div className=" bg-gray-50 bg-cover min-h-screen">  
+//     <Header />
+//       <div className=" pt-10 bg-cover pb-10" style={{ backgroundImage: `url(${cok})`, }}>
+//       <h1 className="text-center pb-10 font-semibold font-display3 text-2xl md:text-3xl lg:text-4xl">What to read next</h1>
+//       <div className="grid md:grid-cols-2 lg:grid-cols-3 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20"> 
+//         {blogposts.map((blogs) => (
+//           <ul key={blogs.id} className="p-3">
+//             <li className=" bg-gray-100 rounded-3xl">
+//               <img className="rounded-t-3xl w-full" src={blogs.image} alt="blog" />
+//               <div className="p-6 lg:p-8 xl:p-10">
+//                 <h1 className="font-semibold line-clamp-1 font-display3 text-xl md:text-2xl lg:text-3xl">{blogs.title}</h1>
+//                 <p className=" line-clamp-3 pt-5 text-base md:text-lg font-display5 lg:text-xl">{blogs.content}</p>
+//               </div>
+//             </li>
+//           </ul>
+//         ))}
+//       </div>
+//       </div>
+//       <div className="p-6 md:p-10">
+//         <section className="block bg-gray-50 bg-cover text-center mx-auto lg:w-1/2 p-4 h-auto border-t-[10px] border-[1px] border-black">
+//           <h1 className="text-xl md:text-3xl 2xl:text-4xl md:pt-5 pb-3 lg:pt-10 font-bold">Sign up for the newsletter</h1>
+//           <h2 className="text-base md:text-xl 2xl:pt-5 px-3 pb-6 pt-3">Sign up your email here to keep update on lastest news about technology.</h2>
+//           <div className="flex md:pt-5 pb-4 md:pb-6 justify-center md:text-xl">
+//             <input className="p-2 border-[1px] xl:px-5 border-black" type="text" placeholder="Email address" />
+//             <button className="px-2 border-black bg-black xl:px-5 text-white border-[2px]">Sign Up</button>
+//           </div>
+//         </section>
+//       </div>
+//       <footer className="pt-10 marquee overflow-hidden">
+//         <Blogfooter />
+//       </footer>
+//     </div>
+//   )
+// }
+
+// export default Blogweb
+
+
+
 import React from 'react'
 import grayball from '../images/grayball.jpg'
 import Blogfooter from './Blogfooter'
 import Pchhor from '../images/Pchhor.jpg'
 import Header from 'Homepage/Header'
+import cok from '../images/cok.jpg'
 
 function Blogweb() {
   const blogposts = [
@@ -59,7 +161,7 @@ function Blogweb() {
   return (
     <div className=" bg-gray-50 bg-cover min-h-screen">  
     <Header />
-      <div className=" pt-10">
+      <div className=" pt-10 bg-cover pb-10" style={{ backgroundImage: `url(${cok})`, }}>
       <h1 className="text-center pb-10 font-semibold font-display3 text-2xl md:text-3xl lg:text-4xl">What to read next</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20"> 
         {blogposts.map((blogs) => (
@@ -93,4 +195,3 @@ function Blogweb() {
 }
 
 export default Blogweb
-
