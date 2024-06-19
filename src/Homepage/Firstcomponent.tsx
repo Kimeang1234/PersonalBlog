15ede0f5a1a25899a1294e728b0a652eefaab07b
LisaOw <kkimeang@gmail.com>
2024-06-19 10:50:47 +0700

    kk

---
+++ src/Homepage/Firstcomponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Firstcomponent() {
+  return (
+    <div className=" min-h-screen">
+      <div className='text-center text-7xl font-display2 pt-48'>comming soon1...</div>
+    </div>
+  )
+}
+
+export default Firstcomponent
---
+++ src/Homepage/Thirdcomponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Thirdcomponenet() {
+  return (
+    <div className=" min-h-screen">
+      <div className='text-center text-7xl font-display2 pt-48'>comming soon3...</div>
+    </div>
+  )
+}
+
+export default Thirdcomponenet
